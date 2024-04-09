1cc52028cb120b4d91fdb29426ff99039a6a6347
Igor Rosa Ramalho <pacfa_sky@hotmail.com>
2024-04-09 12:48:12 -0400

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { TextInput, View, Text} from "react-native";
+
+export default function Header(){
+    return(
+        <View> 
+            <Text>Index</Text>
+        </View>
+    );
+}
---
+++ src/components/Main/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { TextInput, View, Text} from "react-native";
+import Header from '../Header/';
+
+export default function Main(){
+    return(
+        <View> 
+            <Text> me </Text>
+        </View>
+    );
+}
