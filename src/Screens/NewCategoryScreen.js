a4d84a52a1f050657c34f970abffbfca26001825
Igor Rosa Ramalho <pacfa_sky@hotmail.com>
2024-04-28 18:54:58 -0400

    NewCategoryScreen update

---
+++ src/Screens/NewCategoryScreen.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import {
+  Dimensions,
+  Image,
+  Modal,
+  StyleSheet,
+  Text,
+  TextInput, // Ensure TextInput is imported
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+export default function NewCategoryScreen({ closeModal }) {
+  const [newCategoryName, setNewCategoryName] = useState(""); // State for new category name
+
+  const handleSaveCategory = () => {
+    closeModal();
+    // Do something with the newCategoryName
+  };
+
+  return (
+    <Modal
+      animationType="slide"
+      transparent={false}
+      onRequestClose={closeModal}
+    >
+      <View style={styles.viewModal}>
+         <TextInput
+          placeholder="Enter category name"
+          onChangeText={setNewCategoryName}
+          style={styles.input}
+          value={newCategoryName} // Controlled component
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleSaveCategory}
+        >
+          <Text style={styles.buttonText}>Save</Text>
+        </TouchableOpacity> 
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  viewModal: {
+    backgroundColor: "#121212",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    width: 200,
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 16,
+  },
+});
